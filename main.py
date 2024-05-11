7957f3f17002b4327225a9c6ae09e14b53242ec1
NatananPh <nut14586@gmail.com>
2024-05-11 20:18:52 +0700

    improve: add model YOLOv8s

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from ultralytics import YOLO
+import torch
 
 if __name__ == "__main__":
 
     # Load a model
-    model = YOLO("Yolo-Weights/yolov8n.pt")
+    model = YOLO("Yolo-Weights/yolov8s.pt")
+    print(torch.cuda.is_available())
 
     # Train the model
-    results = model.train(data="config.yaml", epochs=100, imgsz=640,)
+    results = model.train(data="config.yaml", epochs=100, imgsz=640)
