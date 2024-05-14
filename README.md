1a9488e390684a0efb4eb81e0f65b0d3c7c92b42
Bhuribhat <Bhuribhat@gmail.com>
2024-05-14 14:39:06 +0700

    add argument parsing

--- README.md
+++ README.md
@@ -1,33 +1,85 @@
-# basketball detection and counting project
+# Basketball Detection And Counting Project
+
 This repository is for 2110433 Computer Vision course Final Project
 
+### Install Requirements
 
-### Problem Statement
+To install run:
+
+```bash
+pip install -r requirements.txt
+```
+
+### Inference
+
+You can run YOLOv8 basketball detection by running the following:
+
+```bash
+$ python shot_detector.py --help
+usage: shot_detector.py [-h] [-s SOURCE] [-vi] [-v]    
+
+options:
+  -h, --help                  show this help message and exit
+  -s SOURCE, --source SOURCE  video/0 for webcam
+  -vi, --view-img             display results
+  -sv, --save-video           save video result
+```
+
+#### Example
+
+```bash
+# For Webcam
+$ python shot_detector.py
+
+# For video
+$ python shot_detector.py --source "/Path/to/Video"
+
+# For viewing real-time result
+$ python shot_detector.py --source "/Path/to/Video" --view-img
+
+# For saving video result in `sample` directory
+$ python shot_detector.py --source "/Path/to/Video" --save-video
+
+# Example detection in sample video with displaying and saving result video
+$ python shot_detector.py --source "sample/multi_angle.mp4" --view-img --save-video
+```
+
+
+## Problem Statement
+
 In basketball training ,manually tracking a single basketball's movement during drills or practice sessions is time-consuming. We aim to develop a computer vision system to automatically detect and count the basketball's movements in real-time video streams, improving efficiency.
-### Technical Challenges
+
+## Technical Challenges
+
 - model accuracy
-  
+
   Based on our solution, we need to determine the position of the basketball relative to the rim, both above and below, to predict whether the basketball successfully enters the goal or not.
 - time/resource for training
 
   Each training session spans approximately 80 epochs and consumes around 400 minutes of computation time on a MacBook Pro M2, which has hardware limitations.
-### Related Works
+
+## Related Works
+
 - https://github.com/chonyy/AI-basketball-analysis
 
   The object detection model is trained with the Faster R-CNN model architecture, which includes pretrained weight on COCO dataset.
 - https://github.com/TwinKay/AI_BasketBall_Video_Analysis
 
   The object detection model is trained with the YOLO-NAS-L, Person Re-Identification is trained with  MobileNetV3 for faster inference time.
-### Method and Results
+
+## Method and Results
+
 - YOLOv8 without Region of interest (ROI)
 - YOLOv8 with Region of interest (ROI)
-### Discussion and Future Work
+
+## Discussion and Future Work
+
 - Due to hardware limitations, it is hard to process real-time data
 - Need more sample images to train our model to increase its accuracy for various basketballs and hoops
 - The current model may detect other object as a basketball which can be fixed by finetuning with negative sample images
-### Future Work
+
+## Future Work
+
 - Person Re-Identification: Calculate the accuracy of each player's shooting during a game
-
 - Pose estimation analysis: Calculate the likelihood for each player based on their shooting form
-
 - Optimize the model: Use ONNX to convert the model weights for faster inference times, enabling real-time video processing
--- shot_detector.py
+++ shot_detector.py
@@ -1,8 +1,10 @@
-from ultralytics import YOLO
+import math
 import cv2
 import cvzone
-import math
+import argparse
 import numpy as np
+
+from ultralytics import YOLO
 from utils import (
     score,
     detect_down,
@@ -14,24 +16,46 @@ from utils import (
 )
 
 
+def parse_opt():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--source', type=str, default=None, help='video/0 for webcam')
+    parser.add_argument('-vi', '--view-img', action='store_true', help='display results')
+    parser.add_argument('-sv', '--save-video', action='store_true', help='save video result')
+    opt = parser.parse_args()
+    return opt
+
+
 class ShotDetector:
-    def __init__(self):
+    def __init__(self, video_path="", view_image=False, save_video=False):
+        self.video_path = video_path if video_path else 0
+        self.view_image = view_image
+        self.save_video = save_video
+
+        if self.video_path != 0:
+            if '/' in self.video_path:
+                self.output_vdo_name = f"{self.video_path.split('/')[-1].split('.')[0]}"
+            else:
+                self.output_vdo_name = self.video_path.split('.')[0]
+        else:
+            self.output_vdo_name = f"webcam_result"
+
+        self.output_writer = cv2.VideoWriter(
+            f"./sample/{self.output_vdo_name}.mp4",
+            cv2.VideoWriter_fourcc(*'mp4v'), 
+            60, (640, 640)
+        )
+
         # Load the YOLO model created from main.py - change text to your relative path
         self.model = YOLO("best.pt")
         self.class_names = ["Basketball", "Basketball Hoop"]
 
-        # Uncomment line below to use webcam (I streamed to my iPhone using Iriun Webcam)
-        # self.cap = cv2.VideoCapture(0)
-
+        # video_path = 0 to use webcam (I streamed to my iPhone using Iriun Webcam)
         # Use video - replace text with your video path
-        self.cap = cv2.VideoCapture("sample/multi_angle.mp4")
+        self.cap = cv2.VideoCapture(self.video_path)
 
-        self.ball_pos = (
-            []
-        )  # array of tuples ((x_pos, y_pos), frame count, width, height, conf)
-        self.hoop_pos = (
-            []
-        )  # array of tuples ((x_pos, y_pos), frame count, width, height, conf)
+        # array of tuples ((x_pos, y_pos), frame count, width, height, conf)
+        self.ball_pos = ([])
+        self.hoop_pos = ([])
 
         self.frame_count = 0
         self.frame = 60
@@ -52,21 +76,23 @@ class ShotDetector:
 
         self.run()
 
+
     def run(self):
         while True:
-            ret, self.frame = self.cap.read()
+            success, self.frame = self.cap.read()
             self.frame = resize_with_pad(self.frame, (640, 640))
 
-            if not ret:
-                # End of the video or an error occurred
+            # End of the video or an error occurred
+            if not success:
                 break
 
             results = self.model(self.frame, stream=True)
 
             for r in results:
                 boxes = r.boxes
+
+                # Bounding box
                 for box in boxes:
-                    # Bounding box
                     x1, y1, x2, y2 = box.xyxy[0]
                     x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
                     w, h = x2 - x1, y2 - y1
@@ -81,8 +107,8 @@ class ShotDetector:
                     center = (int(x1 + w / 2), int(y1 + h / 2))
 
                     # Only create ball points if high confidence or near hoop
-                    if (in_hoop_region(center, self.hoop_pos) and conf > 0.15
-                    ) and current_class == "Basketball":
+                    if (in_hoop_region(center, self.hoop_pos) and conf > 0.15) and \
+                        current_class == "Basketball":
                         self.ball_pos.append((center, self.frame_count, w, h, conf))
                         cvzone.cornerRect(self.frame, (x1, y1, w, h))
 
@@ -96,19 +122,27 @@ class ShotDetector:
             self.display_score()
             self.frame_count += 1
 
-            cv2.imshow("Frame", self.frame)
+            # writing the video frame (.mp4)
+            if self.save_video:
+                self.output_writer.write(self.frame)
+            
+            # Stream results
+            if self.view_image:
+                cv2.imshow("Frame", self.frame)
 
-            # Close if 'q' is clicked
-            if cv2.waitKey(1) & 0xFF == ord(
-                "q"
-            ):  # higher waitKey slows video down, use 1 for webcam
-                break
-
+                # Close if 'q' is clicked
+                # higher waitKey slows video down, use 1 for webcam
+                if cv2.waitKey(1) & 0xFF == ord("q"):  
+                    break
+        
+        self.output_writer.release()
         self.cap.release()
         cv2.destroyAllWindows()
+        print("Finish Detection" + f"in ./sample/{self.output_vdo_name}.mp4" if self.save_video else "..")
 
+
+    # Clean and display ball motion
     def clean_motion(self):
-        # Clean and display ball motion
         self.ball_pos = clean_ball_pos(self.ball_pos, self.frame_count)
         for i in range(0, len(self.ball_pos)):
             cv2.circle(self.frame, self.ball_pos[i][0], 2, (0, 0, 255), 2)
@@ -118,9 +152,10 @@ class ShotDetector:
             self.hoop_pos = clean_hoop_pos(self.hoop_pos)
             cv2.circle(self.frame, self.hoop_pos[-1][0], 2, (128, 128, 0), 2)
 
+
+    # Detecting when ball is in 'up' and 'down' area - ball can only be in 'down' area after it is in 'up'
     def shot_detection(self):
         if len(self.hoop_pos) > 0 and len(self.ball_pos) > 0:
-            # Detecting when ball is in 'up' and 'down' area - ball can only be in 'down' area after it is in 'up'
             if not self.up:
                 self.up = detect_up(self.ball_pos, self.hoop_pos)
                 if self.up:
@@ -149,40 +184,33 @@ class ShotDetector:
                         self.overlay_color = (0, 0, 255)
                         self.fade_counter = self.fade_frames
 
+
+    # Add text
     def display_score(self):
-        # Add text
         text = str(self.makes) + " / " + str(self.attempts)
-        cv2.putText(
-            self.frame,
-            text,
-            (50, 125),
-            cv2.FONT_HERSHEY_SCRIPT_COMPLEX,
-            3,
-            (255, 255, 255),
-            6,
-        )
-        cv2.putText(
-            self.frame,
-            text,
-            (50, 125),
-            cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,
-            3,
-            (0, 0, 0),
-            3,
-        )
+        cv2.putText(self.frame,text, (50, 125), cv2.FONT_HERSHEY_SCRIPT_COMPLEX, 3, (255, 255, 255), 6)
+        cv2.putText(self.frame,text, (50, 125), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 3, (0, 0, 0), 3)
 
         # Gradually fade out color after shot
         if self.fade_counter > 0:
             alpha = 0.2 * (self.fade_counter / self.fade_frames)
             self.frame = cv2.addWeighted(
-                self.frame,
-                1 - alpha,
+                self.frame, 1 - alpha,
                 np.full_like(self.frame, self.overlay_color),
-                alpha,
-                0,
+                alpha, 0
             )
             self.fade_counter -= 1
 
 
 if __name__ == "__main__":
-    ShotDetector()
+    opt = parse_opt()
+    view_image = opt.view_img
+    save_video = opt.save_video
+
+    # Get video source path (0 for webcam)
+    if opt.source == None:
+        video_path = 0
+    else:
+        video_path = opt.source
+
+    ShotDetector(video_path, view_image, save_video)
